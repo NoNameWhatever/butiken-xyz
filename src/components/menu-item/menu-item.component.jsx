690f190582d1ee0928a54077b087038a3c23776d
Fredrik Sundberg <sundrik@gmail.com>
2021-03-10 20:18:56 +0100

    Broken our homepgae into smaller components, finished styling out menu-item component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./menu-item.styles.scss";
+
+const MeuItem = ({ title, imageurl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageurl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MeuItem;
